04265105b5e2e85eaf24aed80fb3fc667a2a829e
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-01 22:41:30 +0300

    Add a solution

---
+++ Inheritance/Inheritance - Lab/P04.RandomList/RandomList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace P04.RandomList
+{
+    public class RandomList : List<string>
+    {
+
+        private Random random;
+        public RandomList()
+        {
+            this.random = new Random();
+        }
+        public string RandomString()
+        {
+            var index = this.random.Next(0, this.Count);
+            var element = this[index];
+            this.RemoveAt(index);
+            return element;
+        }
+    }
+}
---
+++ Inheritance/Inheritance - Lab/P04.RandomList/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using P04.RandomList;
+using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            
+            var randomList = new RandomList();
+            randomList.Add("1");
+            randomList.Add("2");
+            randomList.Add("3");
+            randomList.Add("4");
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
