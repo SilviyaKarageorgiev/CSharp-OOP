9d981ad750b1c6c3c656450626b2abf0d76c2ff3
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-01 23:08:44 +0300

    Add a solution

---
+++ Inheritance/Inheritance - Lab/P05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05.StackOfStrings
+{
+    public class StackOfStrings : Stack<string>
+    {
+        Stack<string> data;
+        public bool IsEmpty()
+        {
+            return this.Count == 0;            
+        }
+
+        public Stack<string> AddRange(IEnumerable<string> data)
+        {
+            foreach (var item in data)
+            {
+                this.Push(item);
+            }
+            return this;
+        }
+    }
+}
---
+++ Inheritance/Inheritance - Lab/P05.StackOfStrings/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using P05.StackOfStrings;
+using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            stackOfStrings.Push("1");
+            stackOfStrings.Push("2");
+            stackOfStrings.Push("3");
+            Console.WriteLine(stackOfStrings.IsEmpty());
+
+            Stack<string> stack = new Stack<string>();
+            stack.Push("11");
+            stack.Push("22");
+            stack.Push("33");
+            stack = stackOfStrings.AddRange(stack);
+
+        }
+    }
+}
