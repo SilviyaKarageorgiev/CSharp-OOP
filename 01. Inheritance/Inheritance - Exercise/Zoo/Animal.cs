4da11195fda50607200b7559364697494a8ba327
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-02 09:25:05 +0300

    Add a solution

---
+++ 01. Inheritance/Inheritance - Exercise/Zoo/Animal.cs
@@ -0,0 +1,14 @@
+﻿namespace Zoo
+{
+    public class Animal
+    {
+        private string name;
+
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
