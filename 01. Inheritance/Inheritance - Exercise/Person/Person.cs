4ab09e3773c365366ff48934cb3e38f05e873978
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-02 09:13:02 +0300

    Add a solution

---
+++ 01. Inheritance/Inheritance - Exercise/Person/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace Person
+{
+    using System;
+    public class Person
+    {
+
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
