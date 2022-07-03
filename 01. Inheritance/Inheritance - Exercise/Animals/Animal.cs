92d0ff981a03c8c270c2cf08a6fcb191fe73cf9d
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-03 22:15:57 +0300

    Add a solution

---
+++ 01. Inheritance/Inheritance - Exercise/Animals/Animal.cs
@@ -0,0 +1,81 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Text;
+
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+        private string gender;
+
+        protected Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.Name), "Name cannot be null or whitespace!");
+                }
+                this.name = value;
+            } 
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException(nameof(this.Age), "Age must be positive number!");
+                }
+                this.age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(nameof(this.Gender), "Gender  cannot be null or whitespace!");
+                }
+                this.gender = value;
+            }
+        }
+
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb
+                .AppendLine($"{this.GetType().Name}")
+                .AppendLine($"{this.Name} {this.Age} {this.Gender}")
+                .AppendLine($"{this.ProduceSound()}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 01. Inheritance/Inheritance - Exercise/Animals/StartUp.cs
@@ -0,0 +1,67 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>();
+
+            string animalType;
+            while ((animalType = Console.ReadLine()) != "Beast!")
+            {
+                try
+                {
+                    string[] animalInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                    string name = animalInfo[0];
+                    int age = int.Parse(animalInfo[1]);
+
+                    Animal animal = null;
+
+                    if (animalType == "Dog")
+                    {
+                        string gender = animalInfo[2];
+                        animal = new Dog(name, age, gender);
+                    }
+                    else if (animalType == "Frog")
+                    {
+                        string gender = animalInfo[2];
+                        animal = new Frog(name, age, gender);
+                    }
+                    else if (animalType == "Cat")
+                    {
+                        string gender = animalInfo[2];
+                        animal = new Cat(name, age, gender);
+                    }
+                    else if (animalType == "Kitten")
+                    {
+                        animal = new Kitten(name, age);
+                    }
+                    else if (animalType == "Tomcat")
+                    {
+                        animal = new Tomcat(name, age);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Invalid input!");
+                    }
+
+                    animals.Add(animal);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+
+            foreach (Animal an in animals)
+            {
+                Console.WriteLine(an.ToString());
+            }
+        }
+    }
+}
