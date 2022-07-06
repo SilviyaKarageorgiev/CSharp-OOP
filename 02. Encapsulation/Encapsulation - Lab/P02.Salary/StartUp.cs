5e40b5eea62ecc61d021325cbf21403131b8f1e3
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-06 23:36:48 +0300

    Add a solution

---
+++ 02. Encapsulation/Encapsulation - Lab/P02.Salary/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new Person(cmdArgs[0],
+                                        cmdArgs[1],
+                                        int.Parse(cmdArgs[2]),
+                                        decimal.Parse(cmdArgs[3]));
+
+                persons.Add(person);
+            }
+
+            var parcentage = decimal.Parse(Console.ReadLine());
+            persons.ForEach(p => p.IncreaseSalary(parcentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+
+        }
+    }
+}
