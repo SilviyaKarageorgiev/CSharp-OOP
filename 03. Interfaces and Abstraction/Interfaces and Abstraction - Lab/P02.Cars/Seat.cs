04653b06039413a6f462d54e86c520afc5da0bec
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-13 17:34:37 +0300

    Add a solutions

---
+++ 03. Interfaces and Abstraction/Interfaces and Abstraction - Lab/P02.Cars/Seat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    internal class Seat : ICar
+    {
+
+        private string model;
+        private string color;
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model}");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 03. Interfaces and Abstraction/Interfaces and Abstraction - Lab/P02.Cars/Tesla.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+
+        private string model;
+        private string color;
+        private int battery;
+
+        public Tesla(string model, string color, int batteries)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = batteries;
+        }
+
+        public int Battery {get; private set;}
+
+        public string Model { get; private set; }
+
+        public string Color { get; private set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
