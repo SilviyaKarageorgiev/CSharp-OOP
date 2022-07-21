870f0c88954691f58ef62fb339633ec40abe22b6
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-21 10:33:08 +0300

    Add a solution

---
+++ 03. Interfaces and Abstraction/Interfaces and Abstraction - Exercise/P01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+        
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+    }
+}
