1ca4cef6b274cf64a030beae64de2a0932854fb3
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-22 00:13:27 +0300

    Change the solution

--- 03. Interfaces and Abstraction/Interfaces and Abstraction - Exercise/P03.Telephony/StartUp.cs
+++ 03. Interfaces and Abstraction/Interfaces and Abstraction - Exercise/P03.Telephony/StartUp.cs
@@ -1,5 +1,7 @@
 ﻿using P03.Telephony.Core;
 using System;
+using Telephony.IO;
+using Telephony.IO.Interfaces;
 
 namespace P03.Telephony
 {
@@ -7,6 +9,13 @@ namespace P03.Telephony
     {
         static void Main(string[] args)
         {
+
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+
+            //IReader fileReader = new FileReader("../../../Storage/data.txt");
+            //IWriter fileWriter = new FileWriter("../../../Storage/result.txt");
+
 
             IEngine engine = new Engine();
             engine.Start();
