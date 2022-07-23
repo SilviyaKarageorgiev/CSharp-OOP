e0f0b9bc1832f1bdf77fb4786bc05de58fd97785
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-23 11:34:35 +0300

    Add a solution

---
+++ 04. Polymorphism/Polymorphism - Lab/P02.Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿namespace Animals
+{
+    public class Animal
+    {
+
+        private string name;
+        private string favouriteFood;
+
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my fovourite food is {this.FavouriteFood}";
+        }
+    }
+}
