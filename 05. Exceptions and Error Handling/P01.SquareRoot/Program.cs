7734d032a7536a98669e1fe7745a67df22268cb8
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-26 15:12:44 +0300

    Add more solutions

---
+++ 05. Exceptions and Error Handling/P01.SquareRoot/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P01.SquareRoot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double number = double.Parse(Console.ReadLine());
+
+                if (number < 0)
+                {
+                    throw new ArgumentException("Invalid number.");
+                }
+
+                Console.WriteLine(CalculateSquareRoot(number));
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine(ae.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
+            
+        }
+
+        private static double CalculateSquareRoot(double number)
+        {
+            return Math.Sqrt(number);
+        }
+    }
+}
---
+++ 05. Exceptions and Error Handling/P02.EnterNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P02.EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int end = 100;
+
+            List<int> numbers = new List<int>();
+
+            while (numbers.Count < 10)
+            {
+                try
+                {
+                    start = ReadNumber(start, end);
+                    numbers.Add(start);
+                }
+                catch(ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+
+            PrintNumbers(numbers);
+        }
+
+        private static void PrintNumbers(List<int> numbers)
+        {
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+
+        public static int ReadNumber(int start, int end)
+        {
+            var curr = Console.ReadLine();
+            if (!int.TryParse(curr, out int result))
+            {
+                throw new ArgumentException("Invalid Number!");
+            }
+            else if (int.Parse(curr) <= start || int.Parse(curr) >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+            return int.Parse(curr);
+        }
+
+    }
+}
