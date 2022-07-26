b2da532051b1aa56519bba1ef984b96c20db07d7
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-26 17:22:16 +0300

    Add a solution

---
+++ 05. Exceptions and Error Handling/P04.SumOfIntegers/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace P04.SumOfIntegers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            string[] input = Console.ReadLine().Split();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string currElement = input[i];
+
+                try
+                {
+                    sum += CheckElementIfItIsInt(currElement);
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine(fe.Message);
+                }
+                catch (OverflowException ofe)
+                {
+                    Console.WriteLine(ofe.Message);
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{currElement}' processed - current sum: {sum}");
+                }
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+
+        }
+
+        private static int CheckElementIfItIsInt(string currElement)
+        {
+            
+            if (!long.TryParse(currElement, out long result))
+            {
+                throw new FormatException($"The element '{currElement}' is in wrong format!");
+            }
+            else if (result < int.MinValue || result > int.MaxValue)
+            {
+                throw new OverflowException($"The element '{currElement}' is out of range!");
+            }
+
+            return (int)result;
+        }
+    }
+}
