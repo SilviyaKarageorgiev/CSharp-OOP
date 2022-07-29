b7575d178bb63ace3734b2b11ea6f47a50557726
Silviya Karageorgieva <s.karageorgieva@yahoo.com>
2022-07-29 16:43:15 +0300

    Add a solution

---
+++ 07. Unit Testing/Unit Testing - Lab/02. TestDummy/Skeleton.Tests/DummyTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class DummyTests
+    {
+        [Test]
+        public void DummyLoosesHealthIfAttacked()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            dummy.TakeAttack(5);
+
+            Assert.AreEqual(5, dummy.Health);
+        }
+
+        [Test]
+        public void DeadDummyThrowsAnExceptionIfAttacked()
+        {
+            Dummy dummy = new Dummy(0, 10);
+
+            Assert.Throws<InvalidOperationException>(() => dummy.TakeAttack(5));
+        }
+
+        [Test]
+        public void DeadDummyCanGiveExpirience()
+        {
+            Dummy dummy = new Dummy(0, 10);
+            int result = dummy.GiveExperience();
+
+            Assert.AreEqual(result, 10);
+        }
+
+        [Test]
+        public void AliveDummyCanGiveExpirience()
+        {
+            Dummy dummy = new Dummy(10, 10);
+            
+            Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+        }
+    }
+}
